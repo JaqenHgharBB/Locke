1cf576ac3d66f444c2d0581caecb8a1b5b24fb4d
JaqenHgharBB <andrerenom@btinternet.com>
2017-07-31 22:20:59 +0100

    Update Simple Moving Average.py

--- Simple Moving Average.py
+++ Simple Moving Average.py
@@ -4,14 +4,14 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-random.seed(1) #For the sake of repeatability, the randomness is seeded, so it is the same each time.
+random.seed(0) #For the sake of repeatability, the randomness is seeded, so it is the same each time.
 
 def serienoise (n): 
     # n is the length of the desired serie
     # This function creates a series based on a cubic with added noise
     s=[]
     for x in range (n):
-        s+=[10*(((0.05*x-2)**3)+2*((0.05*x-2)**2)+1)+random.randint(-100,100)/25]
+        s+=[10*(((0.02*x-2)**3)+2*((0.02*x-2)**2)+1)+random.randint(-200,200)/25]
     return s
     
 def serie (n): 
@@ -19,8 +19,16 @@ def serie (n):
     # This function creates a series based on a cubic with added noise
     s=[]
     for x in range (n):
-        s+=[10*(((0.05*x-2)**3)+2*((0.05*x-2)**2)+1)]
+        s+=[10*(((0.02*x-2)**3)+2*((0.02*x-2)**2)+1)]
     return s
+    
+def meansqrdif (serie1, serie2):
+    # Determines the mean squared differnece between two series
+    # takes two series as inputs and returns a float
+    total=0
+    for x in range (len(serie1)-1):
+        total+=(serie1[x]-serie2[x])**2
+    return total/len(serie1)
 
 ### All the code above is universal to all of the models    
 
@@ -34,18 +42,23 @@ def SMA (serie, n):
         smoothed += [np.mean(s[x:x+(n)])]
     return smoothed
 
-series1 = serienoise(60) 
+series1 = serienoise(150) 
 #This defines a serie length 60 with noise included
-series2 = serie(60)
+series2 = serie(150)
 
-smoothed = SMA(series1,10)
+smoothed = SMA(series1,20)
 #this then smooths that series
 
 ###
 plt.figure(figsize=(10,7.5))
-plt.title('Weighted Moving Average')
+plt.title('Simple Moving Average')
 plt.legend(plot,['Actual Data','Smoothed Values','True Values'])
 ### cosmetic adjustments to the graph
 
 plot = plt.plot(series1, 'r-', smoothed, 'b-', series2, 'g--')
-#Plots on the same axes the original and smoothed series
+#Plots on the same axes the original and smoothed series      
+
+print(meansqrdif(series2, smoothed))
+#prints the mean squared difference of the smothed series compared to the true value
+
+
